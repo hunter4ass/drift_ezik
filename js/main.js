b4bb570b5bf7367bb245339543bc4d530f35b734
hunter4ass <egorilicu@gmail.com>
2025-02-05 17:06:02 +0700

    tre

--- js/main.js
+++ js/main.js
@@ -6,11 +6,47 @@ let app = new Vue({
         description: "A pair of warm, fuzzy socks" ,
         image: "./assets/vmSocks-blue-onWhite.jpg" ,
         altText: "A pair of socks" ,
+        inStock: false,
         inventory: 10 ,
         onSale: "On Sale" ,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'] ,
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+            }
+        ] ,
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'] ,
+        cart: 0 ,
+
+
+
+    } ,
+    methods: {
+        addToCart() {
+            this.cart += 1
+        } ,
+
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+
+        delToCart() {
+            if (this.cart > 0) {
+                this.cart -= 1
+            }
+
+        } ,
 
 
     }
+
 })
 
 
