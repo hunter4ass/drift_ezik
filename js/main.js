3946b8c68165b8c88bc01da821cf1dbc2245e81b
hunter4ass <egorilicu@gmail.com>
2025-02-14 10:30:51 +0700

    tre

--- js/main.js
+++ js/main.js
@@ -21,7 +21,6 @@ Vue.component('task', {
     `
 });
 
-// Определение компонента колонки
 Vue.component('column', {
     props: ['column', 'columnIndex', 'addTask', 'getNextColumnTitle'],
     template: `
@@ -61,7 +60,7 @@ Vue.component('task-return-modal', {
                 <span class="close" @click="closeModal">&times;</span>
                 <h2>{{ isReturn ? 'Укажите причину возврата' : (editingTaskIndex !== null ? 'Редактировать задачу' : 'Добавить задачу') }}</h2>
                 <div v-if="!isReturn">
-                    <input v-model="newTask.title" placeholder="Заголовок задачи" maxlength="30"/>
+                    <input v-model="newTask.title" placeholder="Заголовок задачи" maxlength="30" class="title-task"/>
                     <textarea v-model="newTask.description" class="description-input" placeholder="Описание задачи" maxlength="300"></textarea>
                     <p>Дедлайн:</p>
                     <input type="date" v-model="newTask.deadline" />
@@ -70,7 +69,7 @@ Vue.component('task-return-modal', {
                     </button>
                 </div>
                 <div v-else>
-                    <textarea v-model="newTask.description" class="description-input" placeholder="Причина возврата" maxlength="300"></textarea>
+                    <textarea v-model="newTask.returnReason" class="description-input" placeholder="Причина возврата" maxlength="300"></textarea>
                     <button @click="confirmReturn">Подтвердить возврат</button>
                 </div>
             </div>
@@ -78,7 +77,6 @@ Vue.component('task-return-modal', {
     `
 });
 
-// Основное приложение
 const app = new Vue({
     el: '#app',
     data: {
@@ -91,7 +89,8 @@ const app = new Vue({
         newTask: {
             title: '',
             description: '',
-            deadline: ''
+            deadline: '',
+            returnReason: ''
         },
         showModal: false,
         editingTaskIndex: null,
@@ -100,7 +99,7 @@ const app = new Vue({
         showReturnModal: false
     },
     created() {
-        this.loadData(); // Загружаем данные из localStorage при создании приложения
+        this.loadData();
     },
     methods: {
         loadData() {
@@ -127,18 +126,18 @@ const app = new Vue({
                 this.columns[0].tasks.push(task);
                 this.resetNewTask();
                 this.showModal = false;
-                this.saveData(); // Сохраняем данные после добавления задачи
+                this.saveData();
             }
             this.editingTaskIndex = null;
             this.editingColumnIndex = null;
         },
         deleteTask(columnIndex, taskIndex) {
-            const taskTitle = this.columns[columnIndex].tasks[taskIndex].title; // Получаем заголовок задачи
-            const confirmDelete = confirm(`Вы уверены, что хотите удалить задачу "${taskTitle}"?`); // Запрашиваем подтверждение
+            const taskTitle = this.columns[columnIndex].tasks[taskIndex].title;
+            const confirmDelete = confirm(`Вы уверены, что хотите удалить задачу "${taskTitle}"?`);
 
             if (confirmDelete) {
-                this.columns[columnIndex].tasks.splice(taskIndex, 1); // Удаляем задачу
-                this.saveData(); // Сохраняем данные после удаления задачи
+                this.columns[columnIndex].tasks.splice(taskIndex, 1);
+                this.saveData();
             }
         },
         editTask(columnIndex, taskIndex) {
@@ -156,37 +155,24 @@ const app = new Vue({
                 this.showModal = false;
                 this.editingTaskIndex = null;
                 this.editingColumnIndex = null;
-                this.saveData(); // Сохраняем данные после редактирования задачи
+                this.saveData();
             }
         },
         returnTask(columnIndex, taskIndex) {
             this.editingTaskIndex = taskIndex;
             this.editingColumnIndex = columnIndex;
             this.showReturnModal = true;
-            const task = this.columns[columnIndex].tasks[taskIndex];
-            this.returnReason = ''; // Сбрасываем причину возврата перед вводом
+            this.newTask = { ...this.columns[columnIndex].tasks[taskIndex], returnReason: '' };
         },
-
         confirmReturn() {
             const task = this.columns[this.editingColumnIndex].tasks[this.editingTaskIndex];
-            task.returnReason = this.returnReason; // Присваиваем причину возврата
-            this.moveTask(this.editingColumnIndex, 1, this.editingTaskIndex); // Перемещаем задачу в колонку "Задачи в работе"
-            this.returnReason = ''; // Сбрасываем причину возврата
-            this.showReturnModal = false; // Закрываем модальное окно
-            this.saveData(); // Сохраняем данные после подтверждения возврата
+            task.returnReason = this.newTask.returnReason || 'Не указана';
+            this.moveTask(this.editingColumnIndex, 1, this.editingTaskIndex);
+            this.showReturnModal = false;
+            this.saveData();
         },
-
         moveTask(fromColumnIndex, toColumnIndex, taskIndex) {
             const task = this.columns[fromColumnIndex].tasks.splice(taskIndex, 1)[0];
-
-            // Проверяем, перемещается ли задача из третьего столбца во второй
-            if (fromColumnIndex === 2 && toColumnIndex === 1) {
-                // Если причина возврата существует, присваиваем её полю returnReason
-                task.returnReason = this.returnReason || 'Не указана';
-
-                // Добавляем текст причины возврата в описание задачи
-                task.description += `\nПричина возврата: ${task.returnReason}`; // Добавляем текст причины возврата
-            }
 
             if (toColumnIndex === 3) {
                 const deadlineDate = new Date(task.deadline);
@@ -195,7 +181,7 @@ const app = new Vue({
             }
 
             this.columns[toColumnIndex].tasks.push(task);
-            this.saveData(); // Сохраняем данные после перемещения задачи
+            this.saveData();
         },
         getNextColumnTitle(columnIndex) {
             switch (columnIndex) {
@@ -206,7 +192,12 @@ const app = new Vue({
             }
         },
         resetNewTask() {
-            this.newTask = { title: '', description: '', deadline: '' };
+            this.newTask = {
+                title: '',
+                description: '',
+                deadline: '',
+                returnReason: ''
+            };
         },
         openAddTaskModal() {
             this.resetNewTask();
