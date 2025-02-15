dd409d830d47d2716eacb55838422774f1711e23
hunter4ass <egorilicu@gmail.com>
2025-02-15 12:09:08 +0700

    tre

--- js/main.js
+++ js/main.js
@@ -1,5 +1,39 @@
+Vue.component('user-modal', {
+    props: {
+        showUserModal: Boolean,
+        currentUser: String,
+        closeModal: Function,
+        registerUser: Function
+    },
+    data() {
+        return {
+            username: ''
+        };
+    },
+    template: `
+        <div v-if="showUserModal" class="modal">
+            <div class="modal-content">
+                <span class="close" @click="closeModal">&times;</span>
+                <h2>{{ currentUser ? 'Сменить пользователя' : 'Регистрация' }}</h2>
+                <input v-model="username" placeholder="Введите имя" maxlength="30" class="title-task"/>
+                <button @click="register">
+                    {{ currentUser ? 'Сменить пользователя' : 'Войти' }}
+                </button>
+            </div>
+        </div>
+    `,
+    methods: {
+        register() {
+            if (this.username.trim()) {
+                this.registerUser(this.username);
+                this.username = '';
+            }
+        }
+    }
+});
+
 Vue.component('task', {
-    props: ['task', 'columnIndex', 'taskIndex', 'getNextColumnTitle'],
+    props: ['task', 'columnIndex', 'taskIndex', 'getNextColumnTitle', 'currentUser'],
     template: `
         <div class="task">
             <h3>{{ task.title }}</h3>
@@ -7,22 +41,31 @@ Vue.component('task', {
             <p><strong>Создано:</strong> {{ task.createdAt }}</p>
             <p><strong>Обновлено:</strong> {{ task.updatedAt }}</p>
             <p><strong>Дэдлайн:</strong> {{ task.deadline }}</p>
+            <p><strong>Автор:</strong> {{ task.author }}</p>
             <p v-if="task.returnReason && columnIndex === 1"><strong>Причина возврата:</strong> {{ task.returnReason }}</p>
             <p v-if="task.status"><strong>Статус:</strong> {{ task.status }}</p>
-            <button v-if="columnIndex < 3" @click="$emit('move-task', columnIndex, columnIndex + 1, taskIndex)">
+            <button v-if="columnIndex < 3 && canMoveTask" @click="$emit('move-task', columnIndex, columnIndex + 1, taskIndex)">
                 {{ getNextColumnTitle(columnIndex) }}
             </button>
-            <button v-if="columnIndex === 2" @click="$emit('return-task', columnIndex, taskIndex)">Вернуть в работу</button>
+            <button v-if="columnIndex === 2 && task.author !== currentUser" @click="$emit('return-task', columnIndex, taskIndex)">Вернуть в работу</button>
             <div>
-                <button v-if="columnIndex === 0 || columnIndex === 1 || columnIndex === 2" @click="$emit('edit-task', columnIndex, taskIndex)">Редактировать</button>
-                <button v-if="columnIndex === 0 || columnIndex === 3" @click="$emit('delete-task', columnIndex, taskIndex)">Удалить</button>
+                <button v-if="(columnIndex === 0 || columnIndex === 1 || columnIndex === 2) && task.author === currentUser" @click="$emit('edit-task', columnIndex, taskIndex)">Редактировать</button>
+                <button v-if="(columnIndex === 0 || columnIndex === 3) && task.author === currentUser" @click="$emit('delete-task', columnIndex, taskIndex)">Удалить</button>
             </div>
         </div>
-    `
+    `,
+    computed: {
+        canMoveTask() {
+            if (this.columnIndex === 2) {
+                return this.task.author !== this.currentUser;
+            }
+            return true;
+        }
+    }
 });
 
 Vue.component('column', {
-    props: ['column', 'columnIndex', 'addTask', 'getNextColumnTitle'],
+    props: ['column', 'columnIndex', 'addTask', 'getNextColumnTitle', 'currentUser'],
     template: `
         <div class="column">
             <h2>{{ column.title }}</h2>
@@ -33,12 +76,13 @@ Vue.component('column', {
                 :columnIndex="columnIndex" 
                 :taskIndex="taskIndex" 
                 :getNextColumnTitle="getNextColumnTitle"
+                :currentUser="currentUser"
                 @move-task="$emit('move-task', ...arguments)"
                 @edit-task="$emit('edit-task', ...arguments)"
                 @delete-task="$emit('delete-task', ...arguments)"
                 @return-task="$emit('return-task', ...arguments)"
             ></task>
-            <button v-if="columnIndex === 0" @click="addTask">Добавить задачу</button>
+            <button v-if="columnIndex === 0 && currentUser" @click="addTask">Добавить задачу</button>
         </div>
     `
 });
@@ -90,16 +134,20 @@ const app = new Vue({
             title: '',
             description: '',
             deadline: '',
-            returnReason: ''
+            returnReason: '',
+            author: ''
         },
         showModal: false,
+        showUserModal: false,
         editingTaskIndex: null,
         editingColumnIndex: null,
         returnReason: '',
-        showReturnModal: false
+        showReturnModal: false,
+        currentUser: null
     },
     created() {
         this.loadData();
+        this.loadUser();
     },
     methods: {
         loadData() {
@@ -109,19 +157,31 @@ const app = new Vue({
                 this.columns = parsedData.columns;
             }
         },
+        loadUser() {
+            const savedUser = localStorage.getItem('currentUser');
+            if (savedUser) {
+                this.currentUser = savedUser;
+            }
+        },
         saveData() {
             const dataToSave = {
                 columns: this.columns
             };
             localStorage.setItem('kanbanData', JSON.stringify(dataToSave));
         },
+        registerUser(username) {
+            this.currentUser = username;
+            localStorage.setItem('currentUser', username);
+            this.showUserModal = false;
+        },
         addTask() {
-            if (this.newTask.title) {
+            if (this.newTask.title && this.currentUser) {
                 const task = {
                     ...this.newTask,
                     createdAt: new Date().toLocaleString(),
                     updatedAt: new Date().toLocaleString(),
-                    status: ''
+                    status: '',
+                    author: this.currentUser
                 };
                 this.columns[0].tasks.push(task);
                 this.resetNewTask();
@@ -132,9 +192,13 @@ const app = new Vue({
             this.editingColumnIndex = null;
         },
         deleteTask(columnIndex, taskIndex) {
-            const taskTitle = this.columns[columnIndex].tasks[taskIndex].title;
-            const confirmDelete = confirm(`Вы уверены, что хотите удалить задачу "${taskTitle}"?`);
+            const task = this.columns[columnIndex].tasks[taskIndex];
+            if (task.author !== this.currentUser) {
+                alert('Вы не можете удалить чужую задачу!');
+                return;
+            }
 
+            const confirmDelete = confirm(`Вы уверены, что хотите удалить задачу "${task.title}"?`);
             if (confirmDelete) {
                 this.columns[columnIndex].tasks.splice(taskIndex, 1);
                 this.saveData();
@@ -142,6 +206,11 @@ const app = new Vue({
         },
         editTask(columnIndex, taskIndex) {
             const task = this.columns[columnIndex].tasks[taskIndex];
+            if (task.author !== this.currentUser) {
+                alert('Вы не можете редактировать чужую задачу!');
+                return;
+            }
+
             this.newTask = { ...task };
             this.editingTaskIndex = taskIndex;
             this.editingColumnIndex = columnIndex;
@@ -150,6 +219,11 @@ const app = new Vue({
         saveEditedTask() {
             if (this.newTask.title) {
                 const task = this.columns[this.editingColumnIndex].tasks[this.editingTaskIndex];
+                if (task.author !== this.currentUser) {
+                    alert('Вы не можете редактировать чужую задачу!');
+                    return;
+                }
+
                 Object.assign(task, this.newTask, { updatedAt: new Date().toLocaleString() });
                 this.resetNewTask();
                 this.showModal = false;
@@ -159,10 +233,14 @@ const app = new Vue({
             }
         },
         returnTask(columnIndex, taskIndex) {
+            const task = this.columns[columnIndex].tasks[taskIndex];
+            if (task.author === this.currentUser) {
+            }
+
             this.editingTaskIndex = taskIndex;
             this.editingColumnIndex = columnIndex;
             this.showReturnModal = true;
-            this.newTask = { ...this.columns[columnIndex].tasks[taskIndex], returnReason: '' };
+            this.newTask = { ...task, returnReason: '' };
         },
         confirmReturn() {
             const task = this.columns[this.editingColumnIndex].tasks[this.editingTaskIndex];
@@ -172,7 +250,12 @@ const app = new Vue({
             this.saveData();
         },
         moveTask(fromColumnIndex, toColumnIndex, taskIndex) {
-            const task = this.columns[fromColumnIndex].tasks.splice(taskIndex, 1)[0];
+            const task = this.columns[fromColumnIndex].tasks[taskIndex];
+
+            if (fromColumnIndex === 2 && task.author === this.currentUser) {
+            }
+
+            this.columns[fromColumnIndex].tasks.splice(taskIndex, 1);
 
             if (toColumnIndex === 3) {
                 const deadlineDate = new Date(task.deadline);
@@ -196,10 +279,16 @@ const app = new Vue({
                 title: '',
                 description: '',
                 deadline: '',
-                returnReason: ''
+                returnReason: '',
+                author: ''
             };
         },
         openAddTaskModal() {
+            if (!this.currentUser) {
+                alert('Пожалуйста, войдите в систему, чтобы добавлять задачи!');
+                this.showUserModal = true;
+                return;
+            }
             this.resetNewTask();
             this.editingTaskIndex = null;
             this.editingColumnIndex = null;
@@ -207,31 +296,49 @@ const app = new Vue({
         }
     },
     template: `
-        <div class="kanban-board">
-            <column 
-                v-for="(column, columnIndex) in columns" 
-                :key="columnIndex" 
-                :column="column" 
-                :columnIndex="columnIndex" 
-                :addTask="openAddTaskModal" 
-                :getNextColumnTitle="getNextColumnTitle"
-                @move-task="moveTask"
-                @edit-task="editTask"
-                @delete-task="deleteTask"
-                @return-task="returnTask"
-            ></column>
+        <div>
+            <div class="user-section">
+                <button v-if="!currentUser" @click="showUserModal = true">Войти</button>
+                <div v-else>
+                    <span>Пользователь: {{ currentUser }}</span>
+                    <button @click="showUserModal = true">Сменить пользователя</button>
+                </div>
+            </div>
 
-            <task-return-modal 
-                v-if="showModal || showReturnModal" 
-                :showModal="showModal || showReturnModal" 
-                :newTask="newTask" 
-                :editingTaskIndex="editingTaskIndex" 
-                :returnReason="returnReason" 
-                :isReturn="showReturnModal"
-                :saveTask="editingTaskIndex !== null ? saveEditedTask : addTask" 
-                :confirmReturn="confirmReturn" 
-                :closeModal="() => { showModal = false; showReturnModal = false; }"
-            ></task-return-modal>
+            <div class="kanban-board">
+                <column 
+                    v-for="(column, columnIndex) in columns" 
+                    :key="columnIndex" 
+                    :column="column" 
+                    :columnIndex="columnIndex" 
+                    :addTask="openAddTaskModal" 
+                    :getNextColumnTitle="getNextColumnTitle"
+                    :currentUser="currentUser"
+                    @move-task="moveTask"
+                    @edit-task="editTask"
+                    @delete-task="deleteTask"
+                    @return-task="returnTask"
+                ></column>
+
+                <task-return-modal 
+                    v-if="showModal || showReturnModal" 
+                    :showModal="showModal || showReturnModal" 
+                    :newTask="newTask" 
+                    :editingTaskIndex="editingTaskIndex" 
+                    :returnReason="returnReason" 
+                    :isReturn="showReturnModal"
+                    :saveTask="editingTaskIndex !== null ? saveEditedTask : addTask" 
+                    :confirmReturn="confirmReturn" 
+                    :closeModal="() => { showModal = false; showReturnModal = false; }"
+                ></task-return-modal>
+
+                <user-modal
+                    :showUserModal="showUserModal"
+                    :currentUser="currentUser"
+                    :closeModal="() => showUserModal = false"
+                    :registerUser="registerUser"
+                ></user-modal>
+            </div>
         </div>
     `
 });
